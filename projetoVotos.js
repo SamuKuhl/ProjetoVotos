264588f804f3b3ff40471b97f93858834aeb0247
aluno.lab <aluno.lab@ides.lab>
2024-04-04 16:44:11 -0300

    prontinho

--- projetoVotos.js
+++ projetoVotos.js
@@ -1,7 +1,7 @@
 var contador1 = 0,contador2 = 0, contador3 = 0, contador4 = 0, contador5 = 0, contador6 = 0;
 let escolha
 do {
-    escolha = Number(prompt("Entre com seu voto: \n1 - Zé do Bar\n2 - Maria do bairro\n3 - Tonho\n4 - Fernanda de la paz\n5- Nulo\n6-Branco"))
+    escolha = Number(prompt("Entre com seu voto: \n1 - Zé do Bar\n2 - Maria do bairro\n3 - Tonho\n4 - Fernanda de la paz\n5 - Nulo\n6 - Branco"))
 
     switch (escolha) {
         case 1:
@@ -28,10 +28,19 @@ do {
             alert(`Seu voto foi confirmado para: Branco`);
             contador6++
         break;
+        default : 
+            alert ("Digite um número da lista")
+            contador5++
+        break;
+
     }
-
-    
-
-    console.log (`Zé do Bar encerrou com ${contador1} votos\nMaria do Bairro encerrou com ${contador2} votos\nTonho encerrou com ${contador3} votos\nA porcentagem de votos nulos é de: ${}%\nA porcentagem de votos brancos é de: ${}%`)
     var resposta = Number(prompt("Deseja continuar votando?\n1 - Sim\n2 - Não"));
+
 } while (resposta !== 2);
+
+let totalDeVotos = contador1 + contador2 + contador3 + contador4 + contador5 + contador6
+
+let porcentagemNulo =  (100 * contador5) / totalDeVotos ;
+let porcentagemBranco = (100 * contador6) / totalDeVotos ;
+
+alert (`Zé do Bar encerrou com ${contador1} votos\nMaria do Bairro encerrou com ${contador2} votos\nTonho encerrou com ${contador3} votos\nFernanda de la paz encerrou com ${contador4} votos\nA porcentagem de votos nulos é de: ${porcentagemNulo.toFixed()}%\nA porcentagem de votos brancos é de: ${porcentagemBranco.toFixed()}%`)
